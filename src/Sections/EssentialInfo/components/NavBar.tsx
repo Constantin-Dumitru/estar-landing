aac1bf9ffdc358bf3d4a077fe8901abfb34698c1
Constantin-Dumitru <dumitrubutco11@gmail.com>
2024-08-22 15:45:18 +0300

    hamburger

--- src/Sections/EssentialInfo/components/NavBar.tsx
+++ src/Sections/EssentialInfo/components/NavBar.tsx
@@ -1,22 +1,53 @@
-import React from 'react'
-import BlurContainer from '../../../components/BlurContainer'
-import { EstarLogo } from '../../../shared/icons'
-import NavLinks from './NavLinks'
+import React, { useState } from "react";
+import BlurContainer from "../../../components/BlurContainer";
+import { EstarLogo } from "../../../shared/icons";
+import NavLinks from "./NavLinks";
+import { Stack } from "@mui/material";
 
-const NavBar: React.FC = () => (
+const NavBar: React.FC = () => {
+  const [openNav, setOpenNav] = useState(false)
+  
+  const handleClick = () => (
+    setOpenNav(!openNav)
+  )
+
+  return (
     <BlurContainer
-        sx={{
-            width: '100%',
-            flexDirection: 'row',
-            borderRadius: '1.25rem',
-            padding: '24px',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-        }}
+      sx={{
+        width: "100%",
+        flexDirection: "row",
+        borderRadius: "1.25rem",
+        padding: "24px",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
     >
-        <EstarLogo sx={{ fill: "#F4C501", height: '24px', width: 'auto' }}/>
-        <NavLinks />
+      <EstarLogo sx={{ fill: "#F4C501", height: "24px", width: "auto" }} />
+      {openNav && <NavLinks />}
+      <Stack
+        sx={{
+          gap: "6px",
+          ">*": {
+            background: "#F4C501",
+            width: "2.5rem",
+            height: "3px",
+            borderRadius: "2px",
+            transition: 'all 0.2s ease-in-out'
+          },
+          '& > :nth-of-type(1)': {
+            transform: openNav ? 'rotate(-45deg) translateY(6px)' : 'rotate(0deg)',
+          },
+          '& > :nth-of-type(2)': {
+            transform: openNav ? 'rotate(45deg) translateY(-6px)' : 'rotate(0deg)',
+          }
+        }}
+        onClick={handleClick}
+      >
+        <Stack />
+        <Stack />
+      </Stack>
     </BlurContainer>
-)
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/Sections/EssentialInfo/components/NavLinks.tsx
+++ src/Sections/EssentialInfo/components/NavLinks.tsx
@@ -1,42 +1,67 @@
 import React from "react";
-import { Stack, Link } from "@mui/material";
+import { Stack, Link, Typography } from "@mui/material";
 
 const NavLinks: React.FC = () => (
+  <Stack direction={'row'} gap={'1rem'}>
     <Stack
+      sx={{
+        flexDirection: "row",
+        gap: "1rem",
+        ">*": {
+          fontFamily: "Montserrat, Arial, sans-serif",
+          color: "white",
+          textDecoration: "none",
+          fontWeight: "bold",
+          cursor: "pointer",
+          textTransform: "uppercase",
+          alignItems: "center",
+          justifyContent: "center",
+          display: "flex",
+          flexDirection: "column",
+          "&::after": {
+            content: '" "',
+            width: "0%",
+            background: "#F4C501",
+            height: "2px",
+            display: "flex",
+            transition: "all 0.2s ease-in-out",
+          },
+          "&:hover": {
+            "&::after": {
+              width: "100%",
+            },
+          },
+        },
+      }}
+    >
+      <Link>Link</Link>
+      <Link>Link</Link>
+      <Link>Link</Link>
+    </Stack>
+    <Link
         sx={{
-            flexDirection: 'row',
-            gap: '1rem',
-            '>*': {
-                fontFamily: 'Montserrat, Arial, sans-serif',
-                color: 'white',
-                textDecoration: 'none',
-                fontWeight: 'bold',
-                cursor: 'pointer',
-                textTransform: 'uppercase',
-                alignItems: 'center',
-                justifyContent: 'center',
-                display: 'flex',
-                flexDirection: 'column',
-                '&::after': {
-                    content: '" "',
-                    width: '0%',
-                    background: '#F4C501',
-                    height: '2px',
-                    display: 'flex',
-                    transition: 'all 0.2s ease-in-out'
-                },
-                '&:hover': {
-                    '&::after': {
-                        width: '100%',
-                    }
-                }
-            }
+            textDecoration: 'none'
         }}
     >
-        <Link>Link</Link>
-        <Link>Link</Link>
-        <Link>Link</Link>
-    </Stack>
-)
+      <Stack
+        sx={{
+            background: '#F4C501',
+            borderRadius: '20px',
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: '10px 20px',
+            cursor: 'pointer',
+            border: '2px solid #F4C501',
+            transition: 'all 0.2s ease-in-out',
+            '&:hover': {
+                background: '#FFDD60',
+            }
+        }}
+      >
+        <Typography color={'#00173F'}>ESTAR HUB</Typography>
+      </Stack>
+    </Link>
+  </Stack>
+);
 
-export default NavLinks
+export default NavLinks;
